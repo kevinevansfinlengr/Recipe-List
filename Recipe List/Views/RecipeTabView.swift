403eaa4c66bb880a7d98944542cb96cc2e177ae3
Kevin Evans <kevinevansfinlengr@gmail.com>
2022-03-28 10:04:52 -0400

    Add Recipe Tab View

---
+++ Recipe List/Views/RecipeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeTabView.swift
+//  Recipe List
+//
+//  Created by KEVIN EVANS on 3/27/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
